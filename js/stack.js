8a20f627784fa36936ec9bac7c3b6bd6647fc65d
gauravjohri <gaurav_johri@ymail.com>
2022-01-16 17:47:57 +0530

    changes

--- js/stack.js
+++ js/stack.js
@@ -1,33 +1,53 @@
 class Stack {
+
     collection;
-    constructor(values) {
+    limit;
+
+    constructor(values, limit = 2) {
         this.collection = values;
+        this.limit = limit;
     }
+
     add(element) {
         this.collection.push(element)
     }
+
     remove(index) {
         this.collection.splice(index, 1);
     }
+
     update(index, value) {
         this.collection[index] = value;
     }
+
     get() {
         return this.collection;
     }
+
+    getWithPage() {
+        let page = Math.ceil(this.count() / this.limit)
+        this.collection.page = page;
+        this.collection.totalRecords = this.count();
+        return this.collection;
+    }
+
     getByIindex(index) {
         return this.collection[index];
     }
+
     count() {
         return this.collection.length;
     }
+
     last() {
         return this.collection.pop();
     }
+
     first() {
         return this.collection.shift();
     }
-    search(key, value) {
+
+    searchByKey(key, value) {
         let search = [];
         this.collection.map((element) => {
             if (element.hasOwnProperty(key) && element[key].search(value) !== -1)
@@ -36,6 +56,20 @@ class Stack {
         })
         return search;
     }
+
+    search(value) {
+        let search = [];
+        let found;
+        this.collection.map((obj) => {
+            for (let key in obj) {
+                found = obj[key].toString().indexOf(value) !== -1 ? obj : ''
+                if (found != "")
+                    search.push(found);
+            }
+        })
+        return search;
+    }
+
     has(key) {
         return this.collection.shift().hasOwnProperty(key);
     }
